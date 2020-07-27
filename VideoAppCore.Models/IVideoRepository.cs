2ef2f6db22a17c15b476e1705f04d45ef2652e52
진광철 <tokcjin@gmail.com>
2020-07-27 22:00:33 +0900

    Interface 생성

---
+++ VideoAppCore.Models/IVideoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace VideoAppCore.Models
+{
+    public interface IVideoRepository
+    {
+
+        Video Add(Video model);
+        List<Video> GetVideos();
+        Video GetVideoById(int id);
+        Video UpdateVideo(Video model);
+        void RemoveVideo(int id);
+
+    }
+
+}
---
+++ VideoAppCore.Models/VideoRepositoryAdoNet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VideoAppCore.Models
+{
+    /// <summary>
+    /// [4][1] Ado.net을 이용한 CRUD
+    /// </summary>
+    public class VideoRepositoryAdoNet : IVideoRepository
+    {
+        public Video Add(Video model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Video GetVideoById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Video> GetVideos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveVideo(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Video UpdateVideo(Video model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
