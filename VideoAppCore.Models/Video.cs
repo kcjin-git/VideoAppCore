97e65108eb467f858774bc49c858cc983f428ca3
진광철 <tokcjin@gmail.com>
2020-07-27 21:01:37 +0900

    Model 생성

---
+++ VideoAppCore.Models/Video.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoAppCore.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+    }
+}
